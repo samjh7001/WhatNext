890e4f749cbad2428a0e5abe3cc2cba285f2dc72
samjh7001 <159904948+samjh7001@users.noreply.github.com>
2026-09-04 15:29:11 +0000

    Switching to html and JS

---
+++ program/dataContainer.js
@@ -0,0 +1,18 @@
+//Temporary - will decide how to store this in future
+
+
+{
+  "name": "Helldivers 2",
+  "minPlayers": 1,
+  "maxPlayers": 4,
+  "genre": "Co-op Shooter",
+  "avgSession": 60,
+}
+
+{
+  "name": "Rust",
+  "minPlayers": 1,
+  "maxPlayers": 20,
+  "genre": "PvP ",
+  "avgSession": 60,
+}
